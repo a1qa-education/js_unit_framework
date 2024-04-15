89e1597ef7676dfa77ea1363f1d6dd34fff055b8
Azure DevOps <azure.devops.coe@gmail.com>
2024-04-15 17:48:53 +0300

    initial commit with framework

---
+++ framework/configs/main.wdio.conf.js
@@ -0,0 +1,39 @@
+import path from 'node:path';
+import fs from 'fs-extra';
+import Logger from "../utils/logger.js";
+
+export const downloadDir = path.resolve('./tmp');
+
+export const mainConfig = {
+    runner: 'local',
+    exclude: [
+    ],
+    maxInstances: 1,
+    logLevel: 'warn',
+    bail: 0,
+    waitforTimeout: 0,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    framework: 'mocha',
+    reporters: ['spec', ['allure', { outputDir: 'allure-results' }]],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+
+    onPrepare: function() {
+        fs.ensureDir(downloadDir);
+    },
+
+    after: function (result, capabilities, specs) {
+        fs.emptyDir(downloadDir);
+        Logger.flush();
+    },
+
+    afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+        if (!passed) {
+            await browser.takeScreenshot();
+        }
+    },
+
+}
---
+++ framework/utils/Logger.js
@@ -0,0 +1,40 @@
+import AllureReporter from '@wdio/allure-reporter';
+
+let logBuffer = [];
+export default class Logger {
+  /**
+   * Log info message
+   * @param {string} message Message to log
+   */
+  static info(message) {
+    const msg = `[INFO] ${new Date().toLocaleTimeString()} : ${message}`;
+    console.log(msg);
+    logBuffer.push(msg);
+  }
+
+  /**
+   * Log warning message
+   * @param {string} message Message to log
+   */
+  static warn(message) {
+    const msg = `[WARN] ${new Date().toLocaleTimeString()} : ${message}`;
+    console.log(msg);
+    logBuffer.push(msg);
+  }
+
+  /**
+   * Log error message
+   * @param {string} message Message to log
+   */
+  static error(message) {
+    const msg = `[ERROR] ${new Date().toLocaleTimeString()} : ${message}`;
+    console.log(msg);
+    logBuffer.push(msg);
+    throw new Error(msg);
+  }
+
+  static flush() {
+    AllureReporter.addAttachment('info', logBuffer.join('\n'));
+    logBuffer = [];
+  }
+};
